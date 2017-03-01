938596ec657bb09c383b3031b09b5b47f3e59d40
namaba <one.2426@gmail.com>
2017-03-01 14:22:08 +0900

    プロフィール編集途中

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -5,4 +5,10 @@ class UserProfile < ActiveRecord::Base
   mount_uploader :image2, ImageUploader
   mount_uploader :image3, ImageUploader
   mount_uploader :identification_image, ImageUploader
+
+
+  def age
+    date_format = "%Y%m%d"
+    (Date.today.strftime(date_format).to_i - birthday.strftime(date_format).to_i) / 10000
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   scope '/', module: 'guest' do
     get 'pages/welcome'
 
-    resources :users, only: [:show, :create, :destroy]
+    resources :users, only: [:show, :create, :edit, :update, :destroy]
     resources :searches, only: [:index, :show]
     resources :likes, only: [:index, :show]
     resources :messages, only: [:index, :show, :create]
