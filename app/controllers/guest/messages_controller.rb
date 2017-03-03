b19f94c8e588a27d7e49c13df02bd924a4bb4cee
namaba <one.2426@gmail.com>
2017-03-03 17:55:31 +0900

    メッセージ詳細ページ作成

--- app/controllers/guest/messages_controller.rb
+++ app/controllers/guest/messages_controller.rb
@@ -1,10 +1,18 @@
 class Guest::MessagesController < Guest
+  before_action :set_user, only: [:show]
+
   def index
+
   end
 
   def show
   end
 
   def create
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- app/models/partnership.rb
+++ app/models/partnership.rb
@@ -2,5 +2,21 @@ class Partnership < ActiveRecord::Base
   belongs_to :user
   belongs_to :target, :class_name => "User"
 
-  has_many :user_partnerships
+  has_many :partner_couples, class_name: 'UserPartnership', foreign_key: :partnership_id, dependent: :destroy
+  has_many :couples, through: :partner_couples
+
+
+  #----------------------------------------
+  #  ** Scope **
+  #----------------------------------------
+  scope :not_me, -> (user){ where.not(id: user) }
+  scope :like_me, -> (user){ where(id: Like.select('user_id').where(target_id: user, status: 0))}
+
+
+  #----------------------------------------
+  #  ** InstanceMethod **
+  #----------------------------------------
+  def partner(me)
+    couples.each { |user| return user unless user == me }
+  end
 end
