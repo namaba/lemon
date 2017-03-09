54b69c4c913345bc28a4a2a0ee83dd0833e88667
namaba <one.2426@gmail.com>
2017-03-09 17:21:26 +0900

    murmurcomment追加

---
+++ app/controllers/guest/murmur_comments_controller.rb
@@ -0,0 +1,35 @@
+class Guest::MurmurCommentsController < ApplicationController
+  before_action :comment_params, only: [:create, :update]
+  before_action :set_murmur, only: [:new]
+
+
+  def new
+    @murmur_comment = MurmurComment.new
+  end
+
+  def create
+    @murmur_comment = MurmurComment.create(comment_params)
+    if @murmur_comment.save
+      # 遷移先未定
+      redirect_to murmurs_path, notice: 'つぶやきました'
+    else
+      render :new, notice: 'つぶやけませんでした'
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:murmur_comment).permit(
+      :user_id,
+      :murmur_id,
+      :comment,
+      :status
+      )
+  end
+
+  def set_murmur
+    @murmur = Murmur.find(params[:id])
+  end
+
+
+end
---
+++ test/controllers/guest/murmur_comments_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Guest::MurmurCommentsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
