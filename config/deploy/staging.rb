773b3561e233721e19f79a076ea082584590d115
Kota-Mac <Kouta4750g@gmail.com>
2017-03-22 15:31:08 +0900

    deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,20 +1,20 @@
-# set :rails_env, 'staging'
-# set :unicorn_rack_env, 'staging'
-# set :log_level, :debug
+set :rails_env, 'staging'
+set :unicorn_rack_env, 'staging'
+set :log_level, :debug
 
-# server '54.178.166.243', user: 'b-team', roles: %w{app} #serverのipとuser名は適宜
-# set :ssh_options, keys: '~/.ssh/Lemon_TEST_aws_rsa' #ssh_keyの名前は適宜
+server '54.178.166.243', user: 'b-team', roles: %w{app} #serverのipとuser名は適宜
+set :ssh_options, keys: '~/.ssh/Lemon_TEST_aws_rsa' #ssh_keyの名前は適宜
 
-set :branch, 'master'
+# set :branch, 'master'
 
-role :app, %w{b-team@54.178.166.243}
-role :web, %w{b-team@54.178.166.243}
-role :db,  %w{b-team@54.178.166.243}
+# role :app, %w{b-team@54.178.166.243}
+# role :web, %w{b-team@54.178.166.243}
+# role :db,  %w{b-team@54.178.166.243}
 
-server '54.178.166.243', user: 'b-team', roles: %w{web app db}
+# server '54.178.166.243', user: 'b-team', roles: %w{web app db}
 
-set :ssh_options, {
-    keys: [File.expand_path('~/.ssh/Lemon_TEST_aws_rsa')],
-    forward_agent: true,
-    auth_methods: %w(publickey)
-}
+# set :ssh_options, {
+#     keys: [File.expand_path('~/.ssh/Lemon_TEST_aws_rsa')],
+#     forward_agent: true,
+#     auth_methods: %w(publickey)
+# }
