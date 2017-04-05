85ec81b8bd9a11818bccf84e30c15268bc7039e8
namaba <one.2426@gmail.com>
2017-04-06 01:13:46 +0900

    modalついか

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -7,12 +7,16 @@ $(function(){
   });
 });
 
-
-$(function() {
-  // 2プログレスバーを生成
-  $('#progress').progressbar({
-    value: 37,
-    max: 150
+// modal
+$(function(){
+  $('.btns').click(function(){
+    wn = '.' + $(this).data('tgt');
+    var mW = $(wn).find('.modalBody').innerWidth() / 2;
+    var mH = $(wn).find('.modalBody').innerHeight() / 2;
+    $(wn).find('.modalBody').css({'margin-left':-mW,'margin-top':-mH});
+    $(wn).fadeIn(500);
+  });
+  $('.close,.modalBK').click(function(){
+    $(wn).fadeOut(500);
   });
 });
-
