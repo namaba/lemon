21cf40b716a3a837b44bb70a9d064a2ca60e10ee
namaba <one.2426@gmail.com>
2017-05-21 20:32:45 +0900

    メッセージ作成

--- app/controllers/guest/messages_controller.rb
+++ app/controllers/guest/messages_controller.rb
@@ -2,19 +2,23 @@ class Guest::MessagesController < Guest
   before_action :set_user, only: [:show]
 
   def index
-    @like_users = User.like_me(current_user)
+    # @like_users = User.like_me(current_user)
+    @message = Message.new
+    @partnerships = Partnership.joins(:user).where("user_id = ? or target_id = ?", current_user, current_user)
+    if @messages.nil?
+      @messages = Message.where(partnership_id: @partnerships.first)
+    end
   end
 
   def show
-    @messages = Message.where(partnership_id: params[:id])
-    # @messages = Message.find(5) if @messages.blank?
-    redirect_to likes_path
+    @user = User.find(params[:id])
+    render 'guest/users/preview'
   end
 
   def create
     @message = Message.new(message_params)
     if @message.save!
-      redirect_to likes_path
+      redirect_to :index
     end
   end
 
