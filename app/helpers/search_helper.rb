0c865752db6061124573db71b0c503f2454814cb
namaba <one.2426@gmail.com>
2017-06-14 21:00:34 +0900

    さがす画面デザイン修正

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,11 +1,6 @@
 module SearchHelper
   def good(user)
-    like = Like.find_by(user_id: current_user, target:user)
-    if like.blank?
-        link_to 'イイね', likes_path(id: current_user, target: user), method: :post
-    else
-      'イイね済み'
-    end
+    Like.find_by(user_id: current_user, target:user)
   end
 
   def user_image(image)
