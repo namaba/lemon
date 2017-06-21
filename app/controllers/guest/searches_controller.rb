87115d3c769bac71a6ea86de26e06ffc7f079a3d
namaba <one.2426@gmail.com>
2017-06-21 20:46:26 +0900

    コミット

--- app/controllers/guest/searches_controller.rb
+++ app/controllers/guest/searches_controller.rb
@@ -4,7 +4,7 @@ class Guest::SearchesController < Guest
   def index
     # @users = User.all
     @q = UserProfile.ransack(params[:q])
-    @user_profiles = @q.result.not_me(current_user).like_user(current_user).page(params[:page]).per(8)
+    @user_profiles = @q.result.not_me(current_user).like_user(current_user).page(params[:page]).per(16)
     # @user_profiles = UserProfile.page(params[:page]).per(4)
   end
 
