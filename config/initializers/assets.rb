4bede06ad8879f4c48f23626b61fb2d0bd2b1838
namaba <one.2426@gmail.com>
2017-08-16 19:10:53 +0900

    swiper追加途中

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,5 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( guest.css)
-Rails.application.config.assets.precompile += %w( guest.js admin.js )
+Rails.application.config.assets.precompile += %w( guest.css swiper.min.css)
+Rails.application.config.assets.precompile += %w( guest.js admin.js swiper.min.js)
