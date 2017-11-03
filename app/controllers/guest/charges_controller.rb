9492fd90ef75dcde97a6608a6d8962bf7d66c865
namaba <one.2426@gmail.com>
2017-11-03 18:48:16 +0900

    コイン金額変更

--- app/controllers/guest/charges_controller.rb
+++ app/controllers/guest/charges_controller.rb
@@ -3,26 +3,42 @@ class Guest::ChargesController < Guest
   before_action :find_plan,   only: [:create]
   before_action :set_coin,   only: [:create]
 
+  class NothingCharge < StandardError; end
+
   def new
   end
 
   # プラン変更
   def create
-    raise @plan.inspect
+    raise NothingCharge if @plan.nil && @coin.nil
+
     customer = Stripe::Customer.create(
       :email => params[:stripeEmail], #emailは暗号化されずに受け取れます
       :source  => params[:stripeToken] #めちゃめちゃな文字列です
     )
 
+    if @plan.present?
+      # サブスクリプション作成(支払い発生)
+      subscription = Stripe::Subscription.create(
+        :customer => customer.id,
+        :plan => @plan
+      )
 
-    # サブスクリプション作成(支払い発生)
-    subscription = Stripe::Subscription.create(
-      :customer => customer.id,
-      :plan => @plan
-    )
+      current_user.user_profile.plan = @plan
+      current_user.user_profile.save!
 
-    current_user.user_profile.plan = @plan
-    current_user.user_profile.save!
+    elsif @coin.present? && @amount.present?
+      ###この操作で、決済をします
+      charge = Stripe::Charge.create(
+        :customer    => customer.id,
+        :amount      => @amount,
+        :description => 'Rails Stripe customer',
+        :currency    => 'usd'
+      )
+
+      current_user.pay_coin += @coin
+      current_user.status.save!
+    end
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
@@ -30,51 +46,57 @@ class Guest::ChargesController < Guest
   end
 
   # コイン購入
-  def coin_charge
-    # Amount in cents
-    @amount = 500#引き落とす金額
-    customer = Stripe::Customer.create(
-      :email => params[:stripeEmail], #emailは暗号化されずに受け取れます
-      :source  => params[:stripeToken] #めちゃめちゃな文字列です
-    )
+  # def coin_charge
+  #   # Amount in cents
+  #   @amount = 500#引き落とす金額
+  #   customer = Stripe::Customer.create(
+  #     :email => params[:stripeEmail], #emailは暗号化されずに受け取れます
+  #     :source  => params[:stripeToken] #めちゃめちゃな文字列です
+  #   )
 
-    ###この操作で、決済をします
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'
-    )
+  #   ###この操作で、決済をします
+  #   charge = Stripe::Charge.create(
+  #     :customer    => customer.id,
+  #     :amount      => @amount,
+  #     :description => 'Rails Stripe customer',
+  #     :currency    => 'usd'
+  #   )
 
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to new_charge_path
-  end
+  # rescue Stripe::CardError => e
+  #   flash[:error] = e.message
+  #   redirect_to new_charge_path
+  # end
 
   private
-    def find_plan
-      if params[:plan] == 'one_month'
-        @plan = 'one_month'
-      elsif params[:plan] == 'three_months'
-        @plan = 'three_months'
-      elsif params[:plan] == 'six_months'
-        @plan = 'six_months'
-      elsif params[:plan] == "one_year"
-        @plan = 'one_year'
-      else
-        @plan = nil
-      end
+  def find_plan
+    case params[:plan]
+    when 'one_month'
+      @plan = 'one_month'
+    when 'three_months'
+      @plan = 'three_months'
+    when 'six_months'
+      @plan = 'six_months'
+    when 'one_year'
+      @plan = 'one_year'
+    else
+      @plan = nil
     end
+  end
 
-    def set_coin
-      if params[:coin] == 'ten_coins'
-        @coin = 'ten_coins'
-      elsif params[:coin] == 'fifty_coins'
-        @coin = 'fifty_coins'
-      elsif params[:coiin] == 'one_hundred_coins'
-        @coin = 'one_hundred_coins'
-      else
-        @coin = nil
-      end
+  def set_coin
+    case params[:coin]
+    when 'ten_coins'
+      @coin = 10
+      @amount = 980
+    when 'fifty_coins'
+      @coin = 50
+      @amount = 4980
+    when 'one_hundred_coins'
+      @coin = 100
+      @amount = 6980
+    else
+      @coin = nil
+      @amount = nil
     end
+  end
 end
