f0934fd5b12156c1579aa154e273d368ac137add
namaba <one.2426@gmail.com>
2018-11-08 23:08:16 +0900

    feat: 検索した場合はプロフ充実度順、他のページから遷移してきた場合はログイン順

--- app/controllers/guest/searches_controller.rb
+++ app/controllers/guest/searches_controller.rb
@@ -2,10 +2,9 @@ class Guest::SearchesController < Guest
   before_action :set_user, only: [:show]
 
   def index
-    # @users = User.all
     @q = UserProfile.ransack(params[:q])
     @target_ids = @q.result.not_me(current_user).like_user(current_user).map(&:user_id)
-    @users = User.where(id: @target_ids).includes(:user_profile).order("user_profiles.answer_rate DESC").page(params[:page]).per(16)
+    @users = params[:q].nil? ? order_sign_in_at(@target_ids) : order_answer_rate(@target_ids)
     gon.tutorial_status = current_user.user_profile.tutorial_status
     @tutorial_status = current_user.user_profile.tutorial_status
     # @user_profiles = UserProfile.page(params[:page]).per(4)
@@ -28,5 +27,17 @@ class Guest::SearchesController < Guest
   private
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def base_query(target_ids)
+    User.where(id: target_ids).includes(:user_profile)
+  end
+
+  def order_sign_in_at(target_ids)
+    base_query(target_ids).order("current_sign_in_at DESC").page(params[:page]).per(16)
+  end
+
+  def order_answer_rate(target_ids)
+    base_query(target_ids).order("user_profiles.answer_rate DESC").page(params[:page]).per(16)
   end
 end
