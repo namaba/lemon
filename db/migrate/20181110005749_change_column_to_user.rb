e451316ab922b591d89cf55d0ece60ff8406c3f2
Kota-Mac <Kouta4750g@gmail.com>
2018-11-10 10:15:03 +0900

    [fix]admin_flg_defo

--- db/migrate/20181110005749_change_column_to_user.rb
+++ db/migrate/20181110005749_change_column_to_user.rb
@@ -3,6 +3,7 @@ class ChangeColumnToUser < ActiveRecord::Migration
     change_column :users, :admin_flg, :boolean, default: false, null: false
   end
 
+
   # 変更前の状態
   def down
     change_column :users, :admin_flg, :boolean
