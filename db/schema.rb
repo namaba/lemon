66078dde805304699c6c151e83893ca56b6ddb87
namaba <one.2426@gmail.com>
2018-11-11 17:35:54 +0900

    fix: admin_flgのデフォルト値を変更したmigrationファイルがえらーおこすため修正

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181110005749) do
+ActiveRecord::Schema.define(version: 20181108124149) do
 
   create_table "announcements", force: :cascade do |t|
     t.integer  "user_id",         limit: 4
