a1178de72477663d3a9306b03dcfea921df327f2
Kota-Mac <Kouta4750g@gmail.com>
2018-11-18 19:05:39 +0900

    レビュー後修正

--- db/migrate/20181114103507_change_datatype_hobby_of_userprofiles.rb
+++ db/migrate/20181114103507_change_datatype_hobby_of_userprofiles.rb
@@ -1,6 +1,11 @@
 class ChangeDatatypeHobbyOfUserprofiles < ActiveRecord::Migration
-  def change
+  def up
     change_column :user_profiles, :hobby, :text
     change_column :user_profiles, :personality, :text
+  end
+
+  def down
+    change_column :user_profiles, :hobby, :string
+    change_column :user_profiles, :personality, :string
   end
 end
